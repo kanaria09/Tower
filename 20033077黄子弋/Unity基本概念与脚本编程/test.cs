be8e8ae0fe1ce7433f022ca27bb493cba2c2ffde
20033077黄子弋 <hzy1022zy@163.com>
2022-10-07 11:19:13 +0000

    update 20033077黄子弋/Unity基本概念与脚本编程/test.cs.
    Signed-off-by: 20033077黄子弋 <hzy1022zy@163.com>

--- 20033077黄子弋/Unity基本概念与脚本编程/test.cs
+++ 20033077黄子弋/Unity基本概念与脚本编程/test.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,13 +9,13 @@ public class test : MonoBehaviour
     public GameObject i;
     void Start()
     {
-        GameObject i1 = GameObject.Find("Aparment_Door");   //通过名称获取物体
-        GameObject i2 = GameObject.FindWithTag("Untagged");  //通过标签获取物体
-        GameObject i3 = GameObject.Find("Canvas/DoorTxt");   //通过路径获取物体
-        GameObject i4 = GameObject.Find("Key").GetComponentInChildren<KeyController>();   //获取Key物体下的KeyController组件
-        GameObject i5 = i1.transform.GetChild(0).gameObject;   //获取Aparment_Door下的第一个子对象
-        GameObject i6 = i1.transform.FindChild("SimpleDoor_DoorFrame_LOD0").gameObject;//获取Aparment_Door下名称为SimpleDoor_DoorFrame_LOD0的子对象
-        GameObject.FindObjectOfType<Camera>();   // 获取游戏场景内单个组件
+        GameObject i1 = GameObject.Find("Aparment_Door");   
+        GameObject i2 = GameObject.FindWithTag("Untagged");  
+        GameObject i3 = GameObject.Find("Canvas/DoorTxt");  
+        GameObject i4 = GameObject.Find("Key").GetComponentInChildren<KeyController>();   
+        GameObject i5 = i1.transform.GetChild(0).gameObject;   
+        GameObject i6 = i1.transform.FindChild("SimpleDoor_DoorFrame_LOD0").gameObject;
+        GameObject.FindObjectOfType<Camera>();   
         //获取游戏场景内所有组件
         foreach (var item in GameObject.FindObjectsOfType<Camera>()){
         }
@@ -25,9 +25,9 @@ public class test : MonoBehaviour
         //获取父对象中的组件
         foreach (var item in go.GetComponentsInParent<Camera>()){
         }
-        this.GetComponent<Text>();   //this:当前脚本所依附对象
-        g.transform.parent.Find("Button");  //parent获取父物体
-        g.GetComponent<Button>();   //获取Button组件
+        this.GetComponent<Text>();   
+        i.transform.parent.Find("Button");  
+        ii.GetComponent<Button>();   
     }
   
 }
